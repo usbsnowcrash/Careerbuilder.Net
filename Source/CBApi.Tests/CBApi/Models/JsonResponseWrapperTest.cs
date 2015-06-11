c831b573142d06bd5933db8b1adb058994973530
Justin Rex <Justin.Rex@careerbuilder.com>
2015-06-11 14:09:19 -0400

    add json response wrapper and test

---
+++ Source/CBApi.Tests/CBApi/Models/JsonResponseWrapperTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using CBApi.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using RestSharp.Deserializers;
+
+namespace Tests.com.careerbuilder.CBApi.models {
+
+    [TestClass]
+    public class JsonResponseWrapperTest {
+
+        [TestMethod]
+        [DeploymentItem("testdata\\JsonResponseWrapper.json")]
+        public void DeserializationWorks_WhenPassedRightJSON_Response() {
+            var jsonPath = Path.Combine(Environment.CurrentDirectory, "JsonResponseWrapper.json");
+            var streamReader = new StreamReader(jsonPath);
+            var json = new JsonDeserializer();
+
+            var output = json.Deserialize<JsonWrapper<Name>>(new RestResponse() { Content = streamReader.ReadToEnd() });
+            Assert.AreEqual(output.TotalResults, "1");
+            Assert.AreEqual(output.ReturnedResults, "1");
+            Assert.AreEqual(output.Results[0].Language, "Greek");
+            Assert.AreEqual(output.Results[0].Value, "GR");
+            CollectionAssert.AreEqual(output.Errors, new List<string>());
+            Assert.AreEqual(output.Timestamp, "2015-06-10T14:56:37.3319476-04:00");
+            Assert.AreEqual(output.Status, "Success");
+        }
+    }
+}
