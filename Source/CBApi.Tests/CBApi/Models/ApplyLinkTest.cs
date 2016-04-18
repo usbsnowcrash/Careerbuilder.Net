5f8e722e7ab704b548e157da79bb9b9d5566686e
David-Sharpe <david.sharpe@careerbuilder.com>
2016-04-18 12:02:05 -0400

    Adding ApllyLink tests

---
+++ Source/CBApi.Tests/CBApi/Models/ApplyLinkTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CBApi;
+using CBApi.Framework.Requests;
+using CBApi.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Specialized;
+
+namespace Tests.com.careerbuilder.CBApi.models
+{
+    [TestClass]
+    public class ApplyLinkTest
+    {
+        private static NameValueCollection args;
+        private static ApplyLink applyLink;
+
+        [ClassInitialize]
+        public static void Before(TestContext context)
+        {
+            args = new NameValueCollection() {
+            {"DeveloperKey", "Fake_Key"}, 
+            {"JobDID", "Fake_DID"},
+            {"SiteID", "Fake_Site_ID"},
+            {"HostSite", "US"},
+            {"JApply", "Fake_JApply"},
+            {"ApplicationEmail", "bob@test.org"},
+            {"TrackingID", "Fake_TrackingID"},
+            {"Cobrand", "Fake_Cobrand"}
+            };
+            applyLink = new ApplyLink(args);
+        }
+
+        [TestMethod]
+        public void ApplyLink_Gets_DeveloperKey_From_NameValueCollection()
+        {
+            Assert.AreEqual("Fake_Key", applyLink.DeveloperKey);
+        }
+
+        [TestMethod]
+        public void ApplyLink_Gets_JobDID_From_NameValueCollection()
+        {
+            Assert.AreEqual("Fake_DID", applyLink.JobDID);
+        }
+
+        [TestMethod]
+        public void ApplyLink_Gets_SiteID_From_NameValueCollection()
+        {
+            Assert.AreEqual("Fake_Site_ID", applyLink.SiteID);
+        }
+
+        [TestMethod]
+        public void ApplyLink_Gets_HostStie_From_NameValueCollection()
+        {
+            Assert.AreEqual("US", applyLink.HostSite);
+        }
+
+        [TestMethod]
+        public void ApplyLink_Gets_JApply_From_NameValueCollection()
+        {
+            Assert.AreEqual("Fake_JApply", applyLink.JApply);
+        }
+
+        [TestMethod]
+        public void ApplyLink_Gets_ApplicationEmail_From_NameValueCollection()
+        {
+            Assert.AreEqual("bob@test.org", applyLink.ApplicationEmail);
+        }
+
+        [TestMethod]
+        public void ApplyLink_Gets_TrackingID_From_NameValueCollection()
+        {
+            Assert.AreEqual("Fake_TrackingID", applyLink.TrackingID);
+        }
+
+        [TestMethod]
+        public void ApplyLink_Gets_Cobrand_From_NameValueCollection()
+        {
+            Assert.AreEqual("Fake_Cobrand", applyLink.Cobrand);
+        }
+    }
+}
--- Source/CBApi/CBApi.cs
+++ Source/CBApi/CBApi.cs
@@ -148,7 +148,9 @@ namespace CBApi {
         }
 
         /// <summary>
+        /// Gets the ApplyLink for a Job
         /// </summary>
+        /// <param name="request">A named value collection of params to pass to the api</param>
         /// <returns></returns>
         public string ApplyLink(NameValueCollection request) {
             ApplyLinkRequest applyRequest = new ApplyLinkRequest(request, _Settings);
