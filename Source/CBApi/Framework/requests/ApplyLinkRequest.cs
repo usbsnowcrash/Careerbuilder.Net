26e5bdab35d7e07502e715f46b1517c41276548a
David-Sharpe <david.sharpe@careerbuilder.com>
2016-04-19 13:43:01 -0400

    Dynamically set all params and return Uri

--- Source/CBApi/Framework/requests/ApplyLinkRequest.cs
+++ Source/CBApi/Framework/requests/ApplyLinkRequest.cs
@@ -1,23 +1,25 @@
-﻿using RestSharp;
+﻿using CBApi.Models;
+using RestSharp;
 using System;
-using System.Collections.Generic;
 using System.Collections.Specialized;
-using System.Linq;
-using System.Text;
+using System.Reflection;
 
 namespace CBApi.Framework.Requests
 {
     internal class ApplyLinkRequest : GetRequest
     {
-        protected NameValueCollection args;
+        protected ApplyLink model;
 
-        public ApplyLinkRequest(NameValueCollection args, APISettings settings)
+        public ApplyLinkRequest(NameValueCollection args, APISettings settings) :
+            this(new ApplyLink(args), settings) { }
+
+        public ApplyLinkRequest(ApplyLink args, APISettings settings)
             : base(settings)
         {
             if (args == null) {
                 throw new ArgumentException();
             }
-            this.args = args;
+            model = args;
         }
 
         public override string BaseUrl {
@@ -26,12 +28,18 @@ namespace CBApi.Framework.Requests
 
         public string Retrieve() 
         {
-            _request.AddParameter("JobDID", args["JobDID"]);
+            foreach (PropertyInfo property in model.GetType().GetProperties())
+            {
+                var value = property.GetValue(model, null);
+                if (value != null)
+                {
+                    _request.AddParameter(property.Name, value);
+                }
+            }
             base.BeforeRequest();
             IRestResponse response = _client.Execute(_request);
             _AfterRequestEvent(new RequestEventData(_client, _request, response));
-            //CheckForErrors(response);
-            return response.Content;
+            return response.ResponseUri.ToString();
         }
     }
 }
