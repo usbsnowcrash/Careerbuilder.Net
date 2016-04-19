92aab5cd5a39e774794e69f0535333a01665bf6f
David-Sharpe <david.sharpe@careerbuilder.com>
2016-04-19 11:10:20 -0400

    Fill in method description

--- Source/CBApi/CBApi.cs
+++ Source/CBApi/CBApi.cs
@@ -151,7 +151,7 @@ namespace CBApi {
         /// Gets the ApplyLink for a Job
         /// </summary>
         /// <param name="request">A named value collection of params to pass to the api</param>
-        /// <returns></returns>
+        /// <returns>The link to apply to the given job.</returns>
         public string ApplyLink(NameValueCollection request) {
             ApplyLinkRequest applyRequest = new ApplyLinkRequest(request, _Settings);
             return applyRequest.Retrieve();
